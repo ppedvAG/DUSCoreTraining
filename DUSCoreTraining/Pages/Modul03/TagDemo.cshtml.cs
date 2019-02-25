171cd6f91372247099ac3efd7ebbf1382e6f3dd4
Hannes Preishuber <hannesp@ppedv.de>
2019-02-25 15:54:45 +0100

    Tag Helper Image

---
+++ DUSCoreTraining/Pages/Modul03/TagDemo.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul03
+{
+    public class TagDemoModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

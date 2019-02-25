1c4018dd30c90f2008b0daa42e1d0fc4f51f5caa
Hannes Preishuber <hannesp@ppedv.de>
2019-02-25 16:23:53 +0100

    Page Handler mit Parameter und Route

---
+++ DUSCoreTraining/Pages/Modul03/PageMethode.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul03
+{
+    public class PageMethodeModel : PageModel
+    {
+        public void OnGet()
+        {
+            var x = Request.Query["id"];
+
+
+
+
+        }
+      
+        public void OnGetDrucken([FromRoute] string id)
+        {
+            //[FromQuery] string id
+
+        }
+        //short [FromForm] string id) wie Input name
+        public void OnPost([FromForm(Name ="id")] string myId)
+        {
+          var x=  Request.Form["id"];
+
+        }
+    }
+}

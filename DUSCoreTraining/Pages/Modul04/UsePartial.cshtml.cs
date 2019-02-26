1ec1629f4894adbffaee173eaf8a3d8775cabd9b
Hannes Preishuber <hannesp@ppedv.de>
2019-02-26 10:47:17 +0100

    Partials

---
+++ DUSCoreTraining/Pages/Modul04/UsePartial.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul04
+{
+    public class UsePartialModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public PartialViewResult     OnPost()
+        {
+            return Partial("_Zeit");
+        }
+    }
+}

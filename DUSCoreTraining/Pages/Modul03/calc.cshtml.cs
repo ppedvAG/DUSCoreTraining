333abc0fd258346338512da955c7d6d88e12feb3
Hannes Preishuber <hannesp@ppedv.de>
2019-02-26 07:20:35 +0100

    Taschenrechner HTTP Get Page Handler, Querystring

---
+++ DUSCoreTraining/Pages/Modul03/calc.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul03
+{
+    public class calcModel : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public void OnGet()
+        {
+          
+        }
+        public void OnGetAdd([FromQuery]int eins, [FromQuery] int zwei)
+        {
+            Ergebnis = eins + zwei;
+        }
+
+                public void OnGetSub([FromQuery]int eins, [FromQuery] int zwei)
+        {
+            Ergebnis = eins - zwei;
+        }
+    }
+
+}

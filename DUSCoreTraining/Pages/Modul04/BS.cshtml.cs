f0692506fece3ff57d13d3199429457a4eb1cd9a
Hannes Preishuber <hannesp@ppedv.de>
2019-02-26 10:31:30 +0100

    Boostrap INtro

---
+++ DUSCoreTraining/Pages/Modul04/BS.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul04
+{
+    public class BSModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}

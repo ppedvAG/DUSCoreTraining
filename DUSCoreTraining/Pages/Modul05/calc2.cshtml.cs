f4a4642df6bac29bbdca465405092364f6afd539
Hannes Preishuber <hannesp@ppedv.de>
2019-02-26 12:04:57 +0100

    Taschenrechner POST

---
+++ DUSCoreTraining/Pages/Modul05/calc2.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul05
+{
+    public class calc2Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPostAdd()
+        {
+            Ergebnis =int.Parse( HttpContext.Request.Form["eins"]) +
+                int.Parse(HttpContext.Request.Form["zwei"]);
+        }
+
+        public void OnGPostSub([FromForm]int eins, [FromForm] int zwei)
+        {
+            Ergebnis = eins - zwei;
+
+        }
+    }
+}

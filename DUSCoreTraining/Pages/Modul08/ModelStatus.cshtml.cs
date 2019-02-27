3e66f8aef757b66e7f47bc63e2869ab1f1355be3
Hannes Preishuber <hannesp@ppedv.de>
2019-02-27 18:15:21 +0100

    ModelState Binding Property überschreiben

---
+++ DUSCoreTraining/Pages/Modul08/ModelStatus.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul08
+{
+    public class ModelStatusModel : PageModel
+    {
+        [BindProperty]
+        public string Feld { get; set; }
+        [BindProperty]
+        public string Feld2 { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            ModelState.Remove("Feld2");
+           // Feld = "irgendwas";
+            Feld2 = "neuer Wert";
+            ModelState.Clear();
+        }
+    }
+}

6dc02a19ae9c03a9843be1aff1d109fcda03339b
Hannes Preishuber <hannesp@ppedv.de>
2019-02-27 09:18:39 +0100

    Calc mit Binding

---
+++ DUSCoreTraining/Pages/Modul07/calc3.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DUSCoreTraining.Pages.Modul07
+{
+    public class calc3Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+        [BindProperty]
+        public int eins { get; set; }
+        [BindProperty]
+        public int zwei { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPostAdd()
+        {
+            Ergebnis = eins + zwei;
+        }
+
+        public void OnPostSub()
+        {
+            Ergebnis = eins - zwei;
+
+        }
+    }
+}

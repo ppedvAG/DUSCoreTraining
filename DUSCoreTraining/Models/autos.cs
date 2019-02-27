ac3ccc035632b284ce19490bd2c69d460a6ba67f
Hannes Preishuber <hannesp@ppedv.de>
2019-02-27 10:34:37 +0100

    Drop Downlist Model Binding, Autos, JSON.NET

---
+++ DUSCoreTraining/Models/autos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUSCoreTraining.Models
+{
+   
+
+    public class Autos
+    {
+        public Auto[] Auto { get; set; }
+    }
+
+    public class Auto
+    {
+        public string Marke { get; set; }
+        public Boolean Wahl { get; set; }
+    }
+
+}
---
+++ DUSCoreTraining/Pages/Modul07/AutoAuswahl.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DUSCoreTraining.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+
+namespace DUSCoreTraining.Pages.Modul07
+{
+    public class AutoAuswahlModel : PageModel
+    {
+        public List<Auto> MyAutos { get; set; }
+        public List<SelectListItem> MyItems { get; set; }
+        [BindProperty]
+        public string Auswahl { get; set; }
+        public void OnGet()
+        {
+            var pfad = @"C:\aspnetcore\training\DUSCoreTraining\DUSCoreTraining\wwwroot\app_data\json.json";
+            var json = System.IO.File.ReadAllText(pfad);
+            MyAutos = JsonConvert.DeserializeObject<Autos>(json).Auto.ToList();
+            var q = MyAutos.Select(a => new SelectListItem() { Text = a.Marke, Value = a.Marke });
+
+            var qq = from a in MyAutos
+                     orderby a.Marke
+                     select new SelectListItem
+                     {
+                         Text = a.Marke,
+                         Value = a.Marke
+                     };
+            MyItems = qq.ToList();
+
+        } 
+    }
+}
